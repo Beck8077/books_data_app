8c24dac29e360186eea91895de08e9c79f05dc94
Bekzod Nuriddinov <52420458+Beck8077@users.noreply.github.com>
2025-12-07 14:51:00 +0500

    Update app.py

--- DATA1/app.py
+++ DATA1/app.py
@@ -13,7 +13,7 @@ import streamlit as st
 
 # ================  USERS   ================
 
-df = pd.read_csv('users.csv').drop_duplicates()
+df = pd.read_csv('DATA1/users.csv').drop_duplicates()
 df['phone'] = df['phone'].astype(str).str.replace(r'\D', '', regex=True)
 df['phone'] = df['phone'].apply(lambda x: x[:10].ljust(10, '0'))     # fix short numbers safely
 df['phone'] = df['phone'].apply(lambda x: x[0:3] + '-' + x[3:6] + '-' + x[6:10])
@@ -22,7 +22,7 @@ df = df.fillna('No Info')
 
 # ================  BOOKS   ================
 
-with open('books.yaml', 'r', encoding='utf-8') as read_file:
+with open('DATA1/books.yaml', 'r', encoding='utf-8') as read_file:
     books = yaml.safe_load(read_file)
 
 clean_books = [{k.lstrip(':'): v for k, v in b.items()} for b in books]
@@ -43,7 +43,7 @@ df2["year"] = df2["year"].apply(clean_year)
 
 # ================  ORDERS  ================
 
-df3 = pd.read_parquet('orders.parquet').drop_duplicates()
+df3 = pd.read_parquet('DATA1/orders.parquet').drop_duplicates()
 df3 = df3.fillna('No Info').replace(['NULL', ''], 'No Info')
 
 # cleaning and changing formats in timetable column
@@ -176,3 +176,4 @@ with tab3:
     st.header("Most Popular Author(s)")
     st.write(most_popular_author)
     st.write(f"Sold count: {most_popular_count}")
+
